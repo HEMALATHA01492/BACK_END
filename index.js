67faa45df2795340d7a2044b5958d49cb87ba79e
HEMALATHA01492 <hema01492@gmail.com>
2023-08-03 14:00:54 +0530

    mentor assigning Task

--- index.js
+++ index.js
@@ -1,248 +1,257 @@
 //require models
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const cors=require('cors');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 
+const url = process.env.MONGO_URI;
 
-// ROOMS DATA
-const rooms = [
+mongoose.connect(url)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+
+    // mentor schema
+const mentorSchema = new mongoose.Schema({
+  name: {
+      type: String,
+      required: true
+  },
+  assignedStudents: [
     {
-      roomId: 1,
-      no_of_seats: 100,
-      amenities: "wifi,AC",
-      price_per_hour: 1500,
-      roomName: "ROOM 1",
-    },
-    {
-      roomId: 2,
-      no_of_seats: 150,
-      amenities: "NO-WIFI,NON-AC",
-      price_per_hour: 2000,
-      roomName: "ROOM 2",
-    },
-    {
-      roomId: 3,
-      no_of_seats: 250,
-      amenities: "wifi,AC",
-      price_per_hour: 3000,
-      roomName: "ROOM 3",
-    },
-    {
-        roomId: 4,
-        no_of_seats: 200,
-        amenities: "wifi,NON-AC",
-        price_per_hour: 2000,
-        roomName: "ROOM 4",
-    },
-    {
-        roomId: 5,
-        no_of_seats: 250,
-        amenities: "wifi,NON-AC",
-        price_per_hour: 2500,
-        roomName: "ROOM 5",
+      type: mongoose.Types.ObjectId,
+      ref: "Student"
     }
-  ];
-
-  //booked rooms data
-  const bookRoom = [
-    {
-      bookingId: 1,
-      customerName: "Ganesh",
-      roomId: 1,
-      date: "07-12-2023",
-      start: "08:00",
-      end: "09:00",
-      status: "BOOKED",
-    },
-    {
-      bookingId: 2,
-      customerName: "Nivin",
-      roomId: 1,
-      date: "07-12-2023",
-      start: "08:00",
-      end: "09:00",
-      status: "PENDING",
-    },
-    {
-      bookingId: 3,
-      customerName: "Sree Lakshan",
-      roomId: 4,
-      date: "07-12-2023",
-      start: "08:00",
-      end: "09:00",
-      status: "BOOKED",
-    },
-    {
-        bookingId: 4,
-        customerName: "Sree Lakshan",
-        roomId: 4,
-        date: "15-12-2023",
-        start: "08:00",
-        end: "09:00",
-        status: "BOOKED",
-      },
-      {
-        bookingId: 5,
-        customerName: "Nivin",
-        roomId: 5,
-        date: "17-12-2023",
-        start: "08:00",
-        end: "09:00",
-        status: "BOOKED",
-      },
-  ];
-
-// set the endpoints for Hall Booking
-
-app.get('/', (request, response) => {
-    response.send('<h1> HALL BOOKING API</h1>');
+  ]
 });
 
-// API to show all ROOMS DETAILS
-app.get('/rooms', (request, response) => {
-    response.json(rooms);
+//student schema
+const studentSchema = new mongoose.Schema({
+  name: {
+      type: String,
+      required: true,
+  },
+  currentMentor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Mentor"
+  },
+  previousMentor: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Mentor"
+  }]
 });
 
-//1. creates a new ROOM 
-app.post('/rooms', (request, response) => {
-    const{no_of_seats,amenities, price_per_hour,roomName}=request.body;
 
-    const room = {roomId:rooms.length+1,no_of_seats,amenities, price_per_hour,roomName}
-    rooms.push(room);
-    response.status(201).json({ message: 'Room created successfully' });
+//Create a mentor model
+const Mentor = mongoose.model('Mentor', mentorSchema,'mentors');
+
+//Create a student model
+const Student = mongoose.model('Student', studentSchema,'students');
+
+//Create a mentor
+app.post('/mentor', (request, response) => {
+
+  const { name } = request.body
+  const mentor = new Mentor({ name });
+  mentor.save()
+      .then(mentor => {
+          if (mentor) {
+              response.status(201).json({ message: `Mentor ${name}  created successfully` });
+          }
+      })
+      .catch(err => {
+          response.status(404).json({ err: "Failed to create mentor", "Json-request must include": '{"name":"mentor-name"}' })
+      })
+
+})
+
+// api endpoint for getting all mentors details
+
+/*
+for checking purpose in postman
+https://assign-mentor-wljm.onrender.com/mentor
+*/
+
+app.get("/mentor", (req, res) => {
+  Mentor.find({}, {}).then((mentor) => {
+    res.status(200).json(mentor);
+  });
 });
 
-// fetching a single ROOM Details
-app.get('/rooms/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const room = rooms.find(room => room.roomId === id);
-    if (room) {
-        response.json(room);
-    } else {
-        response.status(404).end('Room id does not exist');
+
+//Create a student
+app.post('/student', (request, response) => {
+
+  const { name } = request.body;
+  const student = new Student({ name });
+  student.save()
+      .then(student => {
+          if (student) {
+              response.status(201).json({ message: `Student ${name}  created successfully` });
+          }
+      })
+      .catch(err => {
+          response.status(404).json({ err: "Failed to create student", "Json input should be": '{"name":"student-name"}' });
+      })
+})
+
+
+//  api endpoint for getting all students details
+
+/*
+for checking purpose in postman
+https://assign-mentor-wljm.onrender.com/student
+*/
+
+app.get("/student", (req, res) => {
+  Student.find({}, {}).then((student) => {
+    res.status(200).json(student);
+  });
+});
+
+
+
+// 03 api endpoint for assigning a student to 
+//3a.one mentor-->mutiple student
+//3b.not allow to assign a mentor if the student already has a mentor
+
+/*
+for checking purpose in postman
+https://assign-mentor-wljm.onrender.com/mentor/64b0131ae06556b8fc27499c/student/64b01246e06556b8fc274994
+*/
+
+app.post("/mentor/:mentorId/student/:studentId", async (req, res) => {
+  try {
+    const { mentorId, studentId } = req.params;
+    const mentor = await Mentor.findById(mentorId);
+    const student = await Student.findById(studentId);
+
+    if (!student || !mentor) {
+      return res.status(404).json({ message: "ID Not found" });
     }
+   
+    if(student.currentMentor){
+     return res.status(404).json({ message: "Mentor Already assigned for this student"})
+    }
+
+    mentor.assignedStudents.push(student);
+    student.currentMentor = mentor;
+    await mentor.save();
+    await student.save();
+    res
+      .status(200)
+      .json({ message: "student assigned to mentor successfully" });
+  }
+  catch (error) {
+    console.log("student assigned to mentor Failed",error);
+  }
+});
+
+//----------------------------------------------------------------------------------------------
+// 04 api endpoint for reassigning a mentor to a student
+//  current mentor --> move to previous mentor
+//  current mentor -->updated to new mentor
+/*
+for checking purpose in postman
+https://assign-mentor-wljm.onrender.com/student/64b01246e06556b8fc274994/mentor/64b0130fe06556b8fc274998
+*/
+
+
+
+app.put("/mentor/:mentorId/student/:studentId", async (req, res) => {
+  try {
+    const { mentorId, studentId } = req.params;
+    const mentor = await Mentor.findById(mentorId);
+    const student = await Student.findById(studentId);
+
+    if (!student || !mentor) {
+      return res.status(404).json({ message: "ID Not found" });
+    }
+   
+    if(student.currentMentor){
+      student.previousMentor.push(student.currentMentor)
+
+    }
+
+    mentor.assignedStudents.push(student);
+    student.currentMentor = mentor;
+    await mentor.save();
+    await student.save();
+    res
+      .status(200)
+      .json({ message: "student assigned to mentor successfully" });
+  }
+  catch (error) {
+    console.log("student assigned to mentor Failed",error);
+  }
 });
 
 
-// API to show  Booking DETAILS
-app.get('/book', (request, response) => {
-    response.json(bookRoom);
-});
 
-//2. BOOK A ROOM 
 
-app.post("/bookRoom", (req, res) => {
-    const { customerName, roomId, date, start, end,  status } = req.body;
-    const bookingFilter = bookRoom.find(
-      (room) => room.date == date && room.roomId == roomId && room.start == start
-    );
-    if (bookingFilter) {
-      return res.status(404).json({ message: "Room already booked" });
-    }
-    let roomIdVerify = rooms.map((room) => (room = room.roomId));
-    if (!roomIdVerify.includes(roomId)) {
-      return res
-        .status(404)
-        .json({ message: "Requested room N/A, Kinldy check Other rooms" });
-    }
-    const booking = {
-      bookingId: bookRoom.length + 1,
-      customerName,
-      date,
-      start,
-      end,
-      roomId,
-      status,
+// 05 api endpoint for showing students of a mentor
+
+/*
+for checking purpose in postman
+https://assign-mentor-wljm.onrender.com/mentor/64b0130fe06556b8fc274998/studentList
+https://assign-mentor-wljm.onrender.com/mentor/64b01315e06556b8fc27499a/studentList
+*/
+
+app.get("/mentor/:mentorId/studentList", async (req, res) => {
+  try {
+    const { mentorId } = req.params;
+    const mentor = await Mentor.findById(mentorId);
+    if (!mentor) return res.status(404).json({ error: "mentor id is not found" });
+    let mentorsStudent = {
+      Name: `${mentor.name}`,
+      StudentList: `${mentor.assignedStudents}`,
     };
-    bookRoom.push(booking);
-    res.status(201).json({ message: "Room booked sucessfully" });
-  });
+    return res.status(200).json(mentorsStudent);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 
-  //3.List of Booked Room
+//06 api endpoint to show previous mentor for a particular student
 
-  app.get("/bookedRoomData", (req, res) => {
-    const bookedRoomData = bookRoom.map((book) => {
-      roomsData = rooms.find((room) => room.roomId == book.roomId);
-      if (book.status == "BOOKED") {
-      return {
-        "Room Name": `${roomsData.roomName}`,
-        "Booked Status": `${book.status}`,
-        "Customer Name": `${book.customerName}`,
-        "Date": `${book.date}`,
-        "Start Time": `${book.start}`,
-        "End Time": `${book.end}`,
-      };}else return;
-    });
-    res.json(bookedRoomData.filter((e)=> e!=null));
-  });
-      
-  //3a.List of NOT Booked Room,Booking Status:not booked or pending
-  app.get("/notbookedRoomData", (req, res) => {
-    const bookedRoomData = bookRoom.map((book) => {
-      roomsData = rooms.find((room) => room.roomId == book.roomId);
-      if (book.status != "BOOKED") {
-      return {
-        "Room Name": `${roomsData.roomName}`,
-        "Booked Status": `${book.status}`,
-        "Customer Name": `${book.customerName}`,
-        "Date": `${book.date}`,
-        "Start Time": `${book.start}`,
-        "End Time": `${book.end}`,
-      };}else return;
-    });
-    res.json(bookedRoomData.filter((e)=> e!=null));
-  });
-      
+/*
+for checking purpose in postman
+https://assign-mentor-wljm.onrender.com/student/64b01230e06556b8fc27498f/previousMentor
+https://assign-mentor-wljm.onrender.com/student/64b0123ce06556b8fc274992/previousMentor
+*/
 
-  //4.List all customers with Booked Data
-
-  app.get("/allcustomers", (req, res) => {
-    const customerData = bookRoom.map((book) => {
-      roomsData = rooms.find((room) => room.roomId == book.roomId);
-      return {
-        "Customer Name": `${book.customerName}`,
-        "Room Name": `${roomsData.roomName}`,
-        "Date": `${book.date}`,
-        "Start Time": `${book.start}`,
-        "End Time": `${book.end}`,
-      };
-    });
-    res.json(customerData);
-  });
-
-  
-  // 5.List how many times customer booked the rooms with customer name
-
-   app.get('/customer/:name', (req, res) => {
-    let CustomerName = req.params.name;
-    let BookedCustomers = bookRoom.filter(customer => customer.customerName.toLowerCase() === CustomerName.toLowerCase())
-     BookedCustomers=BookedCustomers.map((data)=>{
-        let room=rooms.find((e)=>e.roomId == data.roomId);
-        return{
-            "Customer Name": `${data.customerName}`,
-            "Room Name": `${room.roomName}`,
-            "Date": `${data.date}`,
-            "Start Time": `${data.start}`,
-            "End Time": `${data.end}`,
-            "Booking id": `${data.bookingId}`,
-            "Booking date": `${data.date}`,
-            "Booking Status": `${data.status}`
-        };
-     });
-        
-       if (BookedCustomers.length) {
-        res.status(200).json(BookedCustomers);
-        } else {
-        res.status(404).json({ message: "Customer name is not available" });
-       }
-   })
-
-
+app.get("/student/:studentId/previousMentor", async (req, res) => {
+  try {
+    const { studentId } = req.params;
+    if (studentId.length != 24) {
+      return res.status(404).json({ error: "Student id not found" });
+    }
+    const student = await Student.findById(studentId);
+    if (!student.previousMentor) {
+      return res
+        .status(200)
+        .json({ Message: "no previous mentor for this student" });
+    }
+    let PreviousMentor = {
+      "Student Name": `${student.name}`,
+      "Previous Mentor": `${student.previousMentor}`,
+    };
+    res.status(200).json(PreviousMentor);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
  // Listen to the PORT 
   const PORT = 3001;
